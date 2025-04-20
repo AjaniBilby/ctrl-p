3878acd2977940c251b1eb62f2dc9e7e8c092c79
Ajani Bilby <me@ajanibilby.com>
2025-04-20 18:09:11 +1000

    +shortcut support

--- src/index.ts
+++ src/index.ts
@@ -14,23 +14,24 @@
 	element.pallet.appendChild(element.results);
 	element.cover.appendChild(element.pallet);
 
-	const commands = new Array<{ txt: string, element: HTMLElement }>();
 
-	type SearchResult = {
-		command: {
-			txt: string;
-			element: HTMLElement;
-		};
-		segments: Segment[];
-	};
-	const current = {
-		search: "",
-		results: new Array<SearchResult>(),
-		focus: 0
+
+
+
+	const pressed = new Set<string>();
+	const KEY_MAP = {
+		Space:   "space",
+		Control: "ctrl",
+		Escape:  "esc",
 	}
 
 	document.addEventListener("keydown", (ev) => {
 		if (element.cover.isConnected) return;
+
+		pressed.add(KEY_MAP[ev.key as keyof typeof KEY_MAP] || ev.key.toLowerCase());
+		if (pressed.size === 1) FetchCommands();
+		else RefineCommands();
+
 		if (!ev.ctrlKey) return;
 		if (!ev.shiftKey) return;
 		if (ev.key !== "P") return;
@@ -39,6 +40,11 @@
 		ev.stopPropagation();
 		ev.preventDefault();
 		Open();
+	});
+
+	document.addEventListener("keyup", (ev) => {
+		pressed.delete(ev.key === " " ? "space" : ev.key.toLowerCase());
+		FetchCommands();
 	});
 
 	element.input.addEventListener("keydown", (ev) => {
@@ -87,22 +93,50 @@
 
 
 
+
+	const commands = new Array<{ txt: string, element: HTMLElement, shortcut: HTMLDivElement }>();
+	type SearchResult = {
+		command: {
+			txt: string;
+			element: HTMLElement;
+		};
+		segments: Segment[];
+		shortcut: HTMLDivElement;
+	};
+	const current = {
+		search: "",
+		results: new Array<SearchResult>(),
+		focus: 0
+	}
+
 	function Open() {
 		document.body.appendChild(element.cover);
 		element.input.focus();
 		element.input.value = "";
+		pressed.clear();
 
 		commands.length = 0;
 		for (const element of document.querySelectorAll(".cmd")) {
 			if (!(element instanceof HTMLElement))                 continue;
-			if (element.getAttribute("disabled") !== "false")      continue;
-			if (element.getAttribute("data-disabled") !== "false") continue;
+			if (element.getAttribute("disabled") === "false")      continue;
+			if (element.getAttribute("data-disabled") === "false") continue;
 
+			const cmd   = element.getAttribute("data-cmd") || element.getAttribute("cmd");
 			const label = element.getAttribute("aria-label");
 			const dsc   = element.getAttribute("title") || element.innerText;
 
+			const shortcut = document.createElement('div');
+			shortcut.className = "shortcut";
+
+			if (cmd) for (const key of cmd.split(" ")) {
+				const e = document.createElement("kbd");
+				e.innerText = key;
+				shortcut.appendChild(e);
+			}
+
+
 			const txt = (label && dsc) ? `${label}: ${dsc}` : label || dsc || "";
-			commands.push({ txt, element });
+			commands.push({ txt, element, shortcut });
 		}
 
 		current.search = "";
@@ -118,12 +152,12 @@
 	function Search (search: string) {
 		const matching: SearchResult[] = [];
 
-		if (!search) for (const command of commands) matching.push({ command, segments: [{ match: false, str: command.txt }] });
+		if (!search) for (const command of commands) matching.push({ command, segments: [{ match: false, str: command.txt }], shortcut: command.shortcut });
 		else for (const command of commands) {
 			const segments = GetSegments(command.txt, search);
 			if (segments.length < 1) continue;
 
-			matching.push({ command, segments });
+			matching.push({ command, segments, shortcut: command.shortcut });
 		}
 
 		matching.sort((a,b) => {
@@ -156,6 +190,7 @@
 			const result = document.createElement("div");
 			result.className = i++ === 0 ? "result focus" : "result";
 			result.appendChild(label);
+			result.appendChild(match.shortcut);
 			element.results.appendChild(result);
 		}
 	}
@@ -200,4 +235,51 @@
 		if (last && last.match === match) last.str += str;
 		else segments.push({ match, str });
 	}
+
+
+
+
+
+	const shortCuts = new Array<{ keys: Set<string>, element: HTMLElement }>();
+	function FetchCommands() {
+		shortCuts.length = 0;
+		for (const e of document.body.querySelectorAll('[data-cmd], [cmd]')) {
+			if (!(e instanceof HTMLElement)) continue;
+
+			const cmd = e.getAttribute("data-cmd") || e.getAttribute("cmd");
+			if (!cmd) continue;
+
+			const keys = new Set(cmd.toLowerCase().split(" "));
+
+			if (!Subset(pressed, keys)) continue;
+			if (keys.size <= pressed.size) return ActivateShortcut(e);
+
+			shortCuts.push({ keys, element: e });
+		}
+	}
+	function RefineCommands() {
+		const curr = [...shortCuts];
+		for (const opt of curr) {
+			if (!Subset(opt.keys, pressed)) continue;
+
+			if (opt.keys.size <= pressed.size) return ActivateShortcut(opt.element);
+			shortCuts.push(opt);
+		}
+	}
+
+	function ActivateShortcut(e: HTMLElement) {
+		shortCuts.length = 0;
+		pressed.clear();
+		return e.click();
+	}
+
+	function Subset(sub: Set<string>, sup: Set<string>) {
+		for (const e of sub) {
+			if (sup.has(e)) continue;
+			return false;
+		}
+
+		return true;
+	}
+
 })()
